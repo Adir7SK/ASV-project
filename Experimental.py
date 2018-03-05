39d938da622cf8d8b0eaf97bfa213db59a2b09c4
Adir7SK <adir7saly@gmail.com>
2018-03-05 15:17:36 +0100

    Should be the first benchmark on 8 sec

---
+++ Experimental.py
@@ -0,0 +1,80 @@
+# Fibonacci numbers module
+"""
+def fib(n):    # write Fibonacci series up to n
+    a, b = 0, 1
+    while b < n:
+        print(b, end=' ')
+        a, b = b, a+b
+    print()
+
+def fib2(n): #return Fibonacci up to n
+    result = []
+    a, b = 0, 1
+    while n > b :
+        result.append(b)
+        a, b = b, a+b
+    print (result)
+    return result
+"""
+
+
+import time
+def alphabetic_order(one):
+    word = list(one.lower())
+    abword = ""
+    i = 0
+    while len(word) > i:
+        if word[i] != chr(ord('z')+1) and word[i] != " ":
+            w = word[i]
+            d = i
+            for j in range (i, len(word)):
+                if w > word[j] and word[j] != " ":
+                    w = word[j]
+                    d = j
+            abword += w
+            if i != d:
+                i = i-1
+            word[d] = chr(ord('z')+1)
+        i += 1
+    #print (abword)
+    return abword
+
+
+def unique_chars(word):
+
+    unique = ""
+    for i in range (len(word)):
+        unique += chr(ord(word[i])*2 + 2)
+    print (unique)
+    return unique
+
+
+
+def longest_word(sentence):
+    WordsLengths = []
+    i = 0
+    while len(sentence)> i:
+        j = i
+        while len(sentence) > j and sentence[j] != " ":
+            j += 1
+        if j > i:
+            WordsLengths.append(j-i)
+        i = j+1
+    print (WordsLengths)
+
+    longest = 0
+    r, m = 0, 0
+    for i in range(len(WordsLengths)):
+        if WordsLengths[i] > longest:
+            longest = WordsLengths[i]
+            r = m
+        m += WordsLengths[i] + 1
+    ToPrint = ""
+    while len(sentence) > r and sentence[r] != " ":
+        ToPrint += sentence[r]
+        r+=1
+    #print (ToPrint)
+    return ToPrint
+
+def wait():
+    time.sleep(8)
