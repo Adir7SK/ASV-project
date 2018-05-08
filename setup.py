8fe87f7ac0624d7dea14e0043c2370c72232b73d
Adir7SK <31063975+Adir7SK@users.noreply.github.com>
2018-05-08 09:32:43 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ setup(name='ExperimentingSetup',
       description='Experimental setup for asv project',
       author_email='adir7saly@gmail.com',
       url='https://github.com/Adir7SK/ASV-project/',
-      packages=['/users/adir saly kaufmann/Desktop/BeginnerGit/ASV-project/benchmarks'],
+      packages=['/users/adir7/Desktop/asv_proj/benchmarks'],
       )
